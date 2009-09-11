a325b54998a0c347d08ba647f88de3a30c5b6f8e
Harlan Knight <knight@lightning.local>
2009-09-10 21:35:19 -0700

    Validate URL format

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
 	has_many :languages
+  validates_format_of :url, :with => %r{\Ahttp://[A-Za-z0-9][\w./]*\.[\w./]*\Z}
 
 	acts_as_versioned
 end
+                                             

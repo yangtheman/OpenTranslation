d6fa69c10dcf97bb4a9027b295b6898ff4dd7e9e
Harlan Knight <knight@lightning.gateway.2wire.net>
2009-09-13 19:50:31 -0700

    Fix belongs_to relationship between posts and languages

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,4 +1,4 @@
 class Language < ActiveRecord::Base
-	belongs_to :posts
-	belongs_to :post_versions
+	has_many :posts_with_this_orig_lang,   :class_name => 'Post', :foreign_key => 'origin_id'
+	has_many :posts_with_this_target_lang, :class_name => 'Post', :foreign_key => 'ted_id'
 end

0652c3f18bc9cad92d4c08fb2df9d887880f7d3d
yangtheman <yangtheman@gmail.com>
2009-11-05 14:35:58 -0800

    Lose the version specific...

--- config/environment.rb
+++ config/environment.rb
@@ -21,7 +21,7 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "ruby-openid", :lib => "openid"
   config.gem 'will_paginate', :version => '~> 2.3.11', :source => 'http://gemcutter.org'
-  config.gem 'acts_as_ferret', :version => '~> 0.4.4'
+  config.gem 'acts_as_ferret'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named

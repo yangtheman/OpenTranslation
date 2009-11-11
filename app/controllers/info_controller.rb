53b7311948e3d45eb51b020dcbbac170ac0a569b
yangtheman <yangtheman@gmail.com>
2009-11-11 00:23:09 -0800

    Minor bug fixes

---
+++ app/controllers/info_controller.rb
@@ -0,0 +1,8 @@
+class InfoController < ApplicationController
+  def about
+  end
+
+  def tos
+  end
+
+end
--- app/models/orig_post.rb
+++ app/models/orig_post.rb
@@ -4,4 +4,9 @@ class OrigPost < ActiveRecord::Base
   belongs_to :orig_lang, :class_name => 'Language', :foreign_key => 'origin_id'
 
   validates_uniqueness_of :url, :case_sensitive => false
+
+  def self.top(num)
+    orig_cols = OrigPost.column_names.collect {|c| "orig_posts.#{c}"}.join(",")
+    return OrigPost.find_by_sql("SELECT #{orig_cols}, count(posts.id) AS post_count FROM orig_posts LEFT OUTER JOIN posts ON posts.orig_post_id = orig_posts.id GROUP BY orig_posts.id, #{orig_cols} ORDER BY post_count DESC LIMIT #{num}")
+  end
 end

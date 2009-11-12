b3eb705672cd0157f086d08e63512bbfa0f670fa
yangtheman <yangtheman@gmail.com>
2009-11-11 22:13:33 -0800

    Prevent rating own translation

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,7 +3,7 @@ module PostsHelper
     average = (post.rating_average.blank? ? 0 : post.rating_average) * 100 / 5
     html = "<ul class = 'stars'>"
     html += "<li class = 'current_rating' style ='width: #{average}%'>#{average}%</li>"
-    if logged_in? && !post.rated_by?(@current_user) 
+    if logged_in? && !post.rated_by?(@current_user) && post.user_id != @current_user.id 
       html += "<li>#{link_to_remote "One", :update => "post_rating_#{post.id}", :url => rate_post_path(post, :rating => 1), :html => {:class => "one_star"}, :method => :put}</li>"
       html += "<li>#{link_to_remote "Two", :update => "post_rating_#{post.id}", :url => rate_post_path(post, :rating => 2), :html => {:class => "two_stars"}, :method => :put}</li>"
       html += "<li>#{link_to_remote "Three", :update => "post_rating_#{post.id}", :url => rate_post_path(post, :rating => 3), :html => {:class => "three_stars"}, :method => :put}</li>"

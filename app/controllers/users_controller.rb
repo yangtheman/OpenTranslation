81028c3ab9147ca9addbbc6cbc31b747ad98db2b
yangtheman <yangtheman@gmail.com>
2009-11-21 21:37:41 -0800

    Get rid of ununsed codes and edit Title page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,12 @@
 class UsersController < ApplicationController
 
   def edit
-	  @user = @current_user
+    @user = @current_user
   end
 
   def update
-          debugger
-	  @current_user.update_attributes(params[:user])
-	  redirect_to posts_path
+    @current_user.update_attributes(params[:user])
+    redirect_to posts_path
   end
   
   def index

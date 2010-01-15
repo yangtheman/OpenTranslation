868661dba36144c909d3e261261ba13b6b16a037
yangtheman <yangtheman@gmail.com>
2010-01-14 18:57:53 -0800

    Small appreance change

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  before_filter :login_required, :except => :show
+  before_filter :login_required, :except => [:show, :index]
 
   def show
     @user = User.find(params[:id])
